1092cdb274ef39d03b1e106d995326c98c752260
Bernardo Vale <bernardo@kentik.com>
2022-03-08 13:43:13 -0300

    adjust

--- ktools/run.js
+++ ktools/run.js
@@ -1,7 +1,7 @@
 var childProcess = require('child_process');
 const { exit } = require('process');
 const fs = require('fs');
-const mainScript = `main`
+const mainScript = `${__dirname}/main`
 fs.readdirSync(__dirname).forEach(file => {
     console.log(`f: ${file}`);  
 })
