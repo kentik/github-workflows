9044c4c517c8fd39c7ef54fb778e1d0ee8959163
Bernardo Vale <bernardo@kentik.com>
2022-03-08 12:53:50 -0300

    test

--- ktools/run.js
+++ ktools/run.js
@@ -3,4 +3,5 @@ const { exit } = require('process');
 const mainScript = `${__dirname}/action`
 console.log(`path: ${mainScript}`);
 const spawnSyncReturns = childProcess.spawnSync(mainScript, { stdio: 'inherit' })
+console.log(`Final status: ${spawnSyncReturns.status}`);
 exit(spawnSyncReturns.status);
