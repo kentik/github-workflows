ff0a499db7d2f8476a0178f0cb5fc550109bf5f7
Bernardo Vale <bernardo@kentik.com>
2022-03-08 13:54:41 -0300

    cleanup

--- ktools/run.js
+++ ktools/run.js
@@ -2,13 +2,5 @@ var childProcess = require('child_process');
 const { exit } = require('process');
 const fs = require('fs');
 const mainScript = `${__dirname}/main`
-fs.readdirSync(__dirname).forEach(file => {
-    console.log(`f: ${file}`);  
-})
-
-
-
-console.log(`path: ${mainScript}`);
 const spawnSyncReturns = childProcess.spawnSync(mainScript, { stdio: 'inherit' })
-console.log(`Final status: ${spawnSyncReturns.status} err: ${spawnSyncReturns.error}`);
 exit(spawnSyncReturns.status);
