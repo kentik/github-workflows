6170fc2a5881ce9d1ed28db52568a56eb814c19f
Bernardo Vale <bernardo@kentik.com>
2022-03-08 13:01:22 -0300

    test

--- ktools/run.js
+++ ktools/run.js
@@ -1,6 +1,14 @@
 var childProcess = require('child_process');
 const { exit } = require('process');
+const fs = require('fs');
 const mainScript = `${__dirname}/action`
+fs.readdir(__dirname, (err, files) => {
+    files.forEach(file => {
+      console.log(`foo: ${file}`);
+    });
+});
+
+
 console.log(`path: ${mainScript}`);
 const spawnSyncReturns = childProcess.spawnSync(mainScript, { stdio: 'inherit' })
 console.log(`Final status: ${spawnSyncReturns.status} err: ${spawnSyncReturns.error}`);
